9702953ccc8bb860780b8d3456991d1d6fb4939b
Gius993 <giuseppevidili9@gmail.com>
2022-06-10 14:52:47 +0200

    con for

---
+++ jsnak3/js/script.js
@@ -0,0 +1,17 @@
+// - Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+
+
+//FOR
+// let totalSome = 0;
+
+// for (let i = 0; i < 5; i++) {
+// 	let userNumber = parseInt(prompt("inserisci numero"));
+// 	totalSome += userNumber;
+// }
+// console.log(totalSome);
+
+// WHILE
